97813e3ca9c6f51629d38d5f44f0aef384470876
Carlos Acuña Arias <carlos.acuna.arias@est.una.ac.cr>
2025-08-13 18:20:03 -0600

    cambiooooo

--- Project1/practica1.cpp
+++ Project1/practica1.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main() {
 	cout << "Hola mundo";
-	cout << "\nmanana es domingo";
+	cout << "\nmanana es domingo"<<endl;
+	cout << "**********************";
 	return 0;
 }
