ab920f1b3d913bbcbc5b70021447ad5d4a79ce13
Carlos Acuña Arias <carlos.acuna.arias@est.una.ac.cr>
2025-08-13 19:21:01 -0600

    primera parte de la practica

--- Project1/practica1.cpp
+++ Project1/practica1.cpp
@@ -1,9 +1,39 @@
 #include<iostream>
 using namespace std;
 
+class BudgetMatrix {
+private:
+    int rows;    // trimestres
+    int cols;    // 2 columnas: presupuesto y pago
+    double** data;
+
+public:
+    BudgetMatrix(int rows, int cols);
+    ~BudgetMatrix();
+    void setBudget(const double* budgets);
+    void calculatePending(int quarter);
+    double totalPaid(int quarter) const;
+    double pendingPercentage(int quarter) const;
+    void display() const;
+};
+
+BudgetMatrix::BudgetMatrix(int rows, int cols) {
+    this->rows = rows;
+    this->cols = cols;
+    data = new double*[rows];
+    for (int i = 0; i < rows; ++i) {
+        data[i] = new double[cols]{0};
+    }
+}
+BudgetMatrix::~BudgetMatrix() {
+    for (int i = 0; i < rows; ++i) {
+        delete[] data[i];
+    }
+    delete[] data;
+}
+
 int main() {
-	cout << "Hola mundo";
-	cout << "\nmanana es domingo"<<endl;
-	cout << "**********************";
+   
+
 	return 0;
 }
